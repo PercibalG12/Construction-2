8ef68c4c811aa1bac3b2d52922141a5bdd0ff480
PercibalG12 <percipromotionsyndicate.com>
2022-06-14 20:57:57 -0500

    fixed scroll to top btn

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 window.addEventListener('scroll', function (){
     var scroll = document.querySelector('.scrollTop');
     scroll.classList.toggle("active", window,scrollY > 500)
+    if (window,scrollY < 500) {
+         scroll.classList.remove("active") 
+    }
 })
 
 
@@ -9,5 +12,4 @@ window.addEventListener('scroll', function (){
 function scrollToTop(){
     window.scrollTo (0,0);
 }
-
 
